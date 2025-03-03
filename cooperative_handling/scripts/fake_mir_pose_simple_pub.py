538366412b72b90fad52092e5f76af29e5c2daaf
LarsDitt <lars@dittri.ch>
2025-03-03 15:48:17 +0100

    Refactor MUR pose launch file and update fake pose publisher to use Pose type

--- cooperative_handling/scripts/fake_mir_pose_simple_pub.py
+++ cooperative_handling/scripts/fake_mir_pose_simple_pub.py
@@ -3,7 +3,7 @@
 # publishes a fake localization on "mir_pose_simple" topic
 
 import rospy
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import Pose
 from tf import transformations
 from math import pi
 
@@ -14,7 +14,9 @@ class FakeMirPoseSimplePub:
         self.pose_topic = rospy.get_param("~pose_topic", "/mir_pose_simple")
         self.fake_pose = rospy.get_param("~fake_pose", [0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
 
-        self.pose_publisher = rospy.Publisher(self.pose_topic, PoseStamped, queue_size=10)
+        topic = "/" + self.mir_name + self.pose_topic
+
+        self.pose_publisher = rospy.Publisher(topic, Pose, queue_size=10)
         rospy.sleep(1)
         self.run()
 
@@ -26,19 +28,17 @@ class FakeMirPoseSimplePub:
 
 
     def publish_fake_pose(self):
-        fake_pose = PoseStamped()
-        fake_pose.header.stamp = rospy.Time.now()
-        fake_pose.header.frame_id = "map"
-        fake_pose.pose.position.x = self.fake_pose[0]
-        fake_pose.pose.position.y = self.fake_pose[1]
-        fake_pose.pose.position.z = self.fake_pose[2]
-        print(self.fake_pose)
+        fake_pose = Pose()
+        fake_pose.position.x = self.fake_pose[0]
+        fake_pose.position.y = self.fake_pose[1]
+        fake_pose.position.z = self.fake_pose[2]
         q = transformations.quaternion_from_euler(self.fake_pose[3], self.fake_pose[4], self.fake_pose[5])
-        fake_pose.pose.orientation.x = q[0]
-        fake_pose.pose.orientation.y = q[1]
-        fake_pose.pose.orientation.z = q[2]
-        fake_pose.pose.orientation.w = q[3]
+        fake_pose.orientation.x = q[0]
+        fake_pose.orientation.y = q[1]
+        fake_pose.orientation.z = q[2]
+        fake_pose.orientation.w = q[3]
         self.pose_publisher.publish(fake_pose)
+
 
 if __name__ == "__main__":
     rospy.init_node("fake_mir_pose_simple_pub")
